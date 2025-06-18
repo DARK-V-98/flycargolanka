14aac3fac9703c488d359036f7f5c405cd8ca151
Thimira Vishwa <vishwathimira2@gmail.com>
2025-06-18 10:31:39 +0000

    make sure when scrolling this webpage need to be fastes and mooth for al

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import './globals.css';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { Toaster } from "@/components/ui/toaster";
+import { PT_Sans } from 'next/font/google';
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'FlyCargo Lanka',
@@ -17,11 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        {/* Removed direct Google Font links for PT Sans */}
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen">
+      <body className={`${ptSans.className} font-body antialiased flex flex-col min-h-screen`}>
           <Header />
           <main className="flex-grow container mx-auto px-4 py-8">
             {children}
