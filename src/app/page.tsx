96db2c2ccef0ed352977207e4dedb05c20a1d66e
Thimira Vishwa <vishwathimira2@gmail.com>
2025-06-19 12:18:18 +0000

    i have updated the bonanza and wall mart logo wall.png and bonanza.png s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,9 @@ const services = [
 export default function Home() {
   const partnerLogos = [
     { name: "AliExpress", src: "/ali.webp" },
-    { name: "Walmart", src: "/wall.webp", removeBg: true },
+    { name: "Walmart", src: "/wall.png", removeBg: true },
     { name: "Amazon", src: "/amazon.svg" },
-    { name: "Bonanza", src: "/bonanza.jpg", removeBg: true },
+    { name: "Bonanza", src: "/bonanza.png", removeBg: true },
     { name: "eBay", src: "/ebay.svg" },
     { name: "Etsy", src: "/Etsy.svg" },
     { name: "Payoneer", src: "/Payoneer.webp" },
