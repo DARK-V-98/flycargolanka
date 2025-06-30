48de8a06d390fea4d97c32897af216d35a924dc3
Thimira Vishwa <vishwathimira2@gmail.com>
2025-06-30 06:22:19 +0000

    ReferenceError: AlertDialogTrigger is not defined     at https://6000-fi

--- src/app/admin/verify-nic/page.tsx
+++ src/app/admin/verify-nic/page.tsx
@@ -23,6 +23,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import Image from 'next/image';
 import { useToast } from '@/hooks/use-toast';
